79df42e7909c19f9f74b57659789ec504c4f3e7b
myname <2861988ed@gmail.com>
2023-10-30 10:22:19 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  * check_cycle - Checks if a singly linked list has a cycle
+ * check_cycle - Checks if a singly linked list has a cycle
   * @list: Pointer to list to check
   *
   * Return: 1 if a cycle or 0 if not
-  */
+ */
 int check_cycle(listint_t *list)
-{  
-	listint_t *slowly = list, *sonic = list;
-	while (sonic &&sonic->next)
-	(	
-		slowly = slowly->;
-	        sonic = sonic->next->next;
-	        if (slowly == sonic)
-		return (1);
-	)
-	return (0);        
+{
+	listint_t *slowly, *sonic;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	slowly = list;
+	sonic = slowly->next;
+
+	while (slowly != NULL && sonic->next != NULL
+		&& sonic->next->next != NULL)
+	{
+		if (slowly == sonic)
+			return (1);
+		slowly = slowly->next;
+		sonic = sonic->next->next;
+	}
+	return (0);
+}    
