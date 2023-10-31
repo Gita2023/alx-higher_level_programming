70691549d718d4aafd61c31370cdc83898416793
myname <2861988ed@gmail.com>
2023-10-31 10:07:42 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    if x != ord('q') and x != ord('e'):
-        print("{:c}".format(x), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print("{:c}".format(i), end='')
