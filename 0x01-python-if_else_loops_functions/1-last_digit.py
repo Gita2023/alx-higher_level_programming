ee0845313631d3427b819761c590818ec8f036db
myname <2861988ed@gmail.com>
2023-10-31 09:56:16 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-argam = abs(number) % 10
+digit = abs(number) % 10
 if number < 0:
-    argam = -argam
-print("Last argam of {} is {} and is ".format(number, argam), end="")
-if argam > 5:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
     print("greater than 5")
-elif argam == 0:
+elif digit == 0:
     print("0")
 else:
-    print("less than 6 and not 0")")
+    print("less than 6 and not 0")
