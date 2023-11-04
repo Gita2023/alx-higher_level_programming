dc2aad030dea8b237eff37fd213281049b8dbde4
myname <2861988ed@gmail.com>
2023-11-04 01:35:34 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,35 +1,17 @@
-#!/usr/bin/python3
-import ctypes
+#include <Python.h>
 
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
+/**
+ * print_python_list_info - Print basic info about Python lists
+ * @p: The Python object
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size = Py_SIZE(p);
+	int i;
 
-lib.print_python_list_info(l)
-
-del l[1]
-
-lib.print_python_list_info(l)
-
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-
-lib.print_python_list_info(l)
-
-l = []
-
-lib.print_python_list_info(l)
-
-l.append(0)
-
-lib.print_python_list_info(l)
-
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-
-lib.print_python_list_info(l)
-
-l.pop()
-
-lib.print_python_list_info(l)
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %li\n", ((PyListObject *) p)->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %d: %s\n", i,
+		       Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
