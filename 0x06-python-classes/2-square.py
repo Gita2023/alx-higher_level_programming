55cce3225dccadab48ce056d7fcf1146dd464f6b
myname <2861988ed@gmail.com>
2023-11-21 09:06:49 -0800

    0x06. Python - Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """A class that defines a square by size"""
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    """Defines a square"""
+    def __init__(self, size):
+        """Initialises the data"""
         self.__size = size
